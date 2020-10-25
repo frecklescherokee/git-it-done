0155fd802599d17aa1672a786a3ed8ab87c51abd
frecklescherokee <71302040+frecklescherokee@users.noreply.github.com>
2020-10-25 12:09:32 -0500

    made the fetch for user repos dynamic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,26 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
+var getUserRepos = function(user) 
+{
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
-  getUserRepos();
+    // make a request to the url then...
+    fetch(apiUrl).then
+    (
+        // use the response to the fetch call in a function
+        function(response) 
+        {
+            // parse the response with json() then...
+            response.json().then
+            (
+                // use the "data" parsed from the json in a function
+                function(data) 
+                {
+                    // console log the data
+                    console.log(data);
+                }
+            );
+        }
+    );
+};
+  
+getUserRepos();
